35acbcd7a178881c3f375504f3a928b2e627af08
deniseb89 <denise_b89@hotmail.com>
2014-12-16 15:24:50 -0500

    adds annie and john

--- lib/lemurick/lemurs/thareef.rb
+++ lib/lemurick/lemurs/thareef.rb
@@ -1,4 +1,11 @@
 module Lemurick
   class Thareef
+    [
+      "What's cracking, gents?",
+      "I took the lift",
+      "Cheers mate",
+      "I'm going for a smoke",
+      "It's 2 degrees Celcius"
+    ]
   end
 end
