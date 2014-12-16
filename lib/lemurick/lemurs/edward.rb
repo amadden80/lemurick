78a1205a40e43f42edc22b56acb0e7f5eca237d8
tedwardmah <ted.mahoney22@gmail.com>
2014-12-16 15:25:57 -0500

    adds punctuation

--- lib/lemurick/lemurs/edward.rb
+++ lib/lemurick/lemurs/edward.rb
@@ -9,10 +9,9 @@ module Lemurick
         "Moisture is the essence of wetness, and wetness is the essence of beauty.",
         "A mind is a terrible thing.",
         "DINOCOMPARE!!!",
-        "My name is actually Ted",
-        "Ooooo I'm a demure lemure..",
-        "Imitation is the highest form of flatulence",
-        "A pun is the lowest form of humor - when you don't think of it first - Oscar Levant"
+        "Ooooo I'm a demure lemure...",
+        "Imitation is the highest form of flatulence.",
+        "A pun is the lowest form of humor - when you don't think of it first. - Oscar Levant"
       ]
     end
   end
