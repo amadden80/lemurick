ed1f825d0f74a42517dab22647c896bbe85e39a5
deniseb89 <denise_b89@hotmail.com>
2014-12-16 15:12:52 -0500

    matt has words

--- lib/lemurick/lemurs/matt.rb
+++ lib/lemurick/lemurs/matt.rb
@@ -1,4 +1,20 @@
 module Lemurick
   class Matt
+    def speak
+      quotes.sample
+    end
+
+    def quotes
+      [
+        'Comparison is the thief of joy',
+        'Truth is manners, manners are a fiction',
+        'Ho-ho-ho; ha-ha-ha',
+        'I find you to be dubious',
+        'Coconut oil'
+      ].map { |e| e + coconut_oil  }
+    end
+    def coconut_oil
+      "...with coconut oil"
+    end
   end
 end
