3ab42cdae7894a7114fbf85b5acd1cbcd30eab69
deniseb89 <denise_b89@hotmail.com>
2014-12-16 15:29:48 -0500

    adds thareef

--- lib/lemurick/lemurs/thareef.rb
+++ lib/lemurick/lemurs/thareef.rb
@@ -1,11 +1,17 @@
 module Lemurick
   class Thareef
-    [
-      "What's cracking, gents?",
-      "I took the lift",
-      "Cheers mate",
-      "I'm going for a smoke",
-      "It's 2 degrees Celcius"
-    ]
+    def speak
+      quotes.sample
+    end
+
+    def quotes
+      [
+        "What's cracking, gents?",
+        "I took the lift",
+        "Cheers mate",
+        "I'm going for a smoke",
+        "It's 2 degrees Celcius"
+      ]
+    end
   end
 end
