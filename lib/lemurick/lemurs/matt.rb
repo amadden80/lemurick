55706b4b26c72fd67d5afd202d58624ce14c9ac0
Evan <evturn@gmail.com>
2014-12-16 15:11:12 -0500

    first

--- lib/lemurick/lemurs/matt.rb
+++ lib/lemurick/lemurs/matt.rb
@@ -1,4 +1,19 @@
 module Lemurick
   class Matt
+    def speak
+      quotes.sample
+    end
+
+    def quotes
+      [
+        "Comparison is the theif of joy",
+        "Truth is manners; manners are a fiction".
+        "fjkdslasadf",
+        "Dubious"
+      ]
+    end
+    def coconut_oil
+      "...and then I put coconut oil on the kitchen floor, run and then glide like the wind."
+    end
   end
 end
