63804cfd97f18a6c0c3d8954e7706f98b896fefd
PalermoD <palermo.deschamps@gmail.com>
2014-12-16 15:19:04 -0500

    palermo has quotes

--- lib/lemurick/lemurs/palermo.rb
+++ lib/lemurick/lemurs/palermo.rb
@@ -1,4 +1,17 @@
 module Lemurick
   class Palermo
+
+  	def speak
+  		quotes.samples
+  	end 
+
+  	def quotes
+  		[
+  			"I like Rails"
+  			"Rails is great"
+  			"F#$K! why is this not working?!?"
+  			"Life is good"
+  			"When are we going to do more rails stuff?"
+  		]
   end
 end
