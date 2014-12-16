57948f1c7a0580da4abdae53ee34747b12a17383
Poopsik <vadimvny@Vadims-MacBook-Pro.local>
2014-12-16 15:17:20 -0500

    quotes

--- lib/lemurick/lemurs/vadim.rb
+++ lib/lemurick/lemurs/vadim.rb
@@ -1,4 +1,17 @@
 module Lemurick
   class Vadim
+  	def speak 
+  		quotes.samples
+  	end
+  	
+  	def quotes
+  		[
+  			"Yo Andre smoke",
+  			"I am not sure what I am doing",
+  			"Nothing",
+
+  		]
+  	end
+ 
   end
 end
