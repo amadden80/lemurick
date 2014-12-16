20a2c9b761ed38718c30fcfa4646bb4257f88c63
tedwardmah <ted.mahoney22@gmail.com>
2014-12-16 15:22:50 -0500

    adds sayings

--- lib/lemurick/lemurs/edward.rb
+++ lib/lemurick/lemurs/edward.rb
@@ -1,4 +1,19 @@
 module Lemurick
   class Edward
+    def speak
+      quotes.sample
+    end
+
+    def quotes
+      [
+        "Moisture is the essence of wetness, and wetness is the essence of beauty.",
+        "A mind is a terrible thing.",
+        "DINOCOMPARE!!!",
+        "My name is actually Ted",
+        "Ooooo I'm a demure lemure..",
+        "Imitation is the highest form of flatulence",
+        "A pun is the lowest form of humor - when you don't think of it first - Oscar Levant"
+      ]
+    end
   end
 end
