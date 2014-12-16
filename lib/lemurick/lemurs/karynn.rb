ce240c68784dc125337163ca2d19cbc0ef2f4130
karynntran <karynn.tran@gmail.com>
2014-12-16 15:29:07 -0500

    random quotes

--- lib/lemurick/lemurs/karynn.rb
+++ lib/lemurick/lemurs/karynn.rb
@@ -1,4 +1,20 @@
 module Lemurick
   class Karynn
+
+    def speak
+      quotes.sample
+    end
+
+    def quotes
+      [
+        "Queue me for help",
+        "COFFEE",
+        "I can't think of quotes"
+        ":)",
+        ":D",
+        "^_^"
+      ]
+    end
+
   end
 end
