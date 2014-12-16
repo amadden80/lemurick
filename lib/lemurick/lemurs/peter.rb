a087237b736ffa27aecf4ca85dc433bd292054f7
pnmichalakis <pnmichalakis@gmail.com>
2014-12-16 15:23:13 -0500

    peter's quotes

--- lib/lemurick/lemurs/peter.rb
+++ lib/lemurick/lemurs/peter.rb
@@ -1,4 +1,15 @@
 module Lemurick
   class Peter
+  	def speak
+  		quotes.sample
+  	end
+  	def quotes
+  		[
+  			"Bruce Springsteen sucks",
+  			"We should all aspire to be a little more like Danza",
+  			"It's unbelievable that the President was once named Herbert",
+  			"As Thomas Aquinas said, there shall come a time when a man is not measured by his deeds, but by the size and magnitude of his pompadour"
+  		]
+  	end
   end
 end
