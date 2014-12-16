a275e041cfd4da321d2fee554f6bed4cc7c86879
Holderness <andersonholderness@gmail.com>
2014-12-16 15:22:40 -0500

    adsfasdf

--- lib/lemurick/lemurs/anderson.rb
+++ lib/lemurick/lemurs/anderson.rb
@@ -1,4 +1,18 @@
 module Lemurick
   class Anderson
+  	def speak
+  		quotes.sample
+  	end
+
+  	def quotes
+  		[
+  			"The untold tragedy of the Titanic is the iceburg.",
+  			"Forest Gump could run, but he couldn't run that fast.",
+  			"There's something appealing about a cross between a Nascar fan and a beluga whale.",
+  			"Cheesey Biscuits."
+
+  		]
+  	end
+
   end
 end
--- lib/lemurick/lemurs/matt.rb
+++ lib/lemurick/lemurs/matt.rb
@@ -1,23 +1,4 @@
 module Lemurick
   class Matt
-
-  	def speak
-  		quotes.sample
-  	end
-
-  	def quotes
-  		[
-  			"Comparison is the thief of joy",
-  			"Truth is manners; manners are a fiction",
-  			"Ho ho ho; ha ha ha",
-  			"Wisdom in youth is an abomination",
-  			"Dubious",
-  		].map{|string| string + coconut_oil}
-  	end
-
-  	def coconut_oil
-  		"...with coconut oil"
-  	end
-  	
   end
 end
