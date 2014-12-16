8f4b298a48d322b6fb83413e96652f70ff618f5e
RadWells <scwells@icloud.com>
2014-12-16 15:11:56 -0500

    I have words

--- lib/lemurick/lemurs/conrad.rb
+++ lib/lemurick/lemurs/conrad.rb
@@ -1,4 +1,18 @@
 module Lemurick
   class Conrad
+    def speak
+      quotes.sample
+    end
+
+    def quotes
+      [
+        'Shhhh',
+        'Fist to Five plus Six',
+        'Gameplay',
+        'Sweet Christmas',
+        'Denise? Denise??',
+    
+      ]
+    end
   end
 end
