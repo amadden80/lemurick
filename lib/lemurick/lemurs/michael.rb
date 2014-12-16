61215ddd834610892dd4aa0a99357debd5e4aed1
CarsonBills <CarsonMBills@Gmail.com>
2014-12-16 15:23:56 -0500

    Mike says things now

--- lib/lemurick/lemurs/michael.rb
+++ lib/lemurick/lemurs/michael.rb
@@ -1,4 +1,16 @@
 module Lemurick
   class Michael
+    def speak
+      quotes.sample
+    end
+    def quotes
+      [
+        "...",
+        "I'm still working on auto-complete.",
+        "I just wasted an hour on 'fucntion'",
+        "Your code works now.",
+        "I'm going to soul-fixins",
+      ]
+    end
   end
 end
