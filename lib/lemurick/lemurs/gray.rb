924739e6043c99ff94e67eeafc6499a012236fcd
grayyeargin <grayyeargin@gmail.com>
2014-12-16 15:29:17 -0500

    best quotes ever said by human mostly about jezzball

--- lib/lemurick/lemurs/gray.rb
+++ lib/lemurick/lemurs/gray.rb
@@ -1,4 +1,16 @@
 module Lemurick
   class Gray
+    def speak
+      quotes.sample
+    end
+
+    def quotes
+      [
+        "it\'s wierd man!",
+        "who got to "+ rand(4..8) + " jezzballs?",
+        "something something jezzballssszzz"
+      ]
+    end
+
   end
 end
