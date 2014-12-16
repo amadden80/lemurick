48b0ca10eac816b19b4468ab011701e964836abc
thaynsworth <thomas.haynsworth@gmail.com>
2014-12-16 15:10:58 -0500

    set up matts quotes

--- lib/lemurick/lemurs/matt.rb
+++ lib/lemurick/lemurs/matt.rb
@@ -1,4 +1,23 @@
 module Lemurick
   class Matt
+
+    def speak
+      quotes.samples
+    end
+
+    def quotes
+      [
+        "Comparison is the thief of joy",
+        "Truth is manners; manners are a fiction",
+        "ho ho ho; ha ha ha",
+        "Wisdom in youth is an abomination",
+        "Dubious"
+      ].map{|string| string + coconut_oil}
+    end
+
+    def coconut_oil
+      "...loves coconut oil"
+    end
+
   end
 end
